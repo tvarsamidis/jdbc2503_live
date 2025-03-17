e1bf4d40d37cb0f3adfce4a81fcb6855a0b9a2c6
Thomas the Great <no email yet>
2025-03-17 11:55:38 +0200

    Add server start and stop

--- src/main/java/gr/codehub/jdbc/demo02/DbTestMyCrudOperations.java
+++ src/main/java/gr/codehub/jdbc/demo02/DbTestMyCrudOperations.java
@@ -2,17 +2,40 @@ package gr.codehub.jdbc.demo02;
 
 import org.h2.tools.Server;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.time.LocalDate;
+import java.util.Properties;
 
 public class DbTestMyCrudOperations {
     // H2
     private static Server server;
+    private static Properties sqlCommands = new Properties();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         loadProperties();
+        startH2Server();
+        stopH2Server();
     }
 
-    private static void loadProperties() {
+    private static void stopH2Server() {
+        if (server.isRunning(true)) {
+            server.stop();
+            server.shutdown();
+            System.out.println("H2 server has shut down");
+        }
+    }
+
+    private static void startH2Server() throws Exception {
+        System.out.println("Starting H2 server");
+        server = Server.createWebServer("-web", "-webAllowOthers");
+        server.start();
+        System.out.println("H2 server has started with status: " + server.getStatus());
+    }
+
+    private static void loadProperties() throws Exception {
+        InputStream inputStream = new FileInputStream("data/demo02.properties");
+        sqlCommands.load(inputStream);
     }
 
 
